ad7a79401d8f2730f4303c322813f095a7cdb55f
andresroa <AndresMore1423@hotmail.com>
2023-09-07 17:48:26 -0500

    Prueba de integración

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Servidor actualizado con server scrapper" });
+});
+
 app.listen(port, () =>
   console.log(`Application run in http://localhost:${port}`)
 );
