8832ac9ce18966909de7fe937919fb73a7220682
andresroa <AndresMore1423@hotmail.com>
2023-09-07 17:49:54 -0500

    Prueba de integración

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Servidor actualizado con server scrapper" });
+  res.json({ message: "Servidor actualizado con server scrapper by Tauro Dev" });
 });
 
 app.listen(port, () =>
