a61c5a4cea45c13390c5672d435030664e209bb4
andresroa <AndresMore1423@hotmail.com>
2023-09-07 18:11:03 -0500

    -deploy Sprint 7

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Gracias nexos por la confianza" });
+  res.json({ message: "Sprint 7 finalizado" });
 });
 
 app.listen(port, () =>
