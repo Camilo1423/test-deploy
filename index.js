5d4f6d3271d09bc83125e88e7516fe7a6f03da47
andresroa <AndresMore1423@hotmail.com>
2023-09-08 08:39:21 -0500

    -deploy Prueba de despliegue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Server deploy continue Nexos Finished" });
+  res.json({ message: "Server deploy continue Nexos Finished con deploy" });
 });
 
 app.listen(port, () =>
