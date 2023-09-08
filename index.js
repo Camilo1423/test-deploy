703ebace81ac8204a8dcc278c295d5a81e4d843a
andresroa <AndresMore1423@hotmail.com>
2023-09-08 08:38:35 -0500

    Prueba de despliegue

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Sprint 7 finalizado second commit" });
+  res.json({ message: "Server deploy continue Nexos Finished" });
 });
 
 app.listen(port, () =>
